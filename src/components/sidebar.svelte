6885a8c963e4262534187f406f2f77117452f9b1
Santilio Nicolo <santilio@aurigaactive.aurigainformatica.it>
2023-11-02 18:05:55 +0100

    shadcn

---
+++ src/components/sidebar.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    import {Button} from "$lib/components/ui/button";
+</script>
+
+<div class="space-y-4 py-4">
+    <div class="px-3 py-2">
+        <h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">
+            Discover
+        </h2>
+        <div class="space-y-1">
+            <Button variant="secondary" class="w-full justify-start">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-home w-4 h-4 mr-2">
+                    <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+                    <polyline points="9 22 9 12 15 12 15 22"/>
+                </svg>
+                Home
+            </Button>
+            <Button variant="ghost" class="w-full justify-start">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                     class="lucide lucide-calendar-days mr-2 w-4 h-4">
+                    <rect width="18" height="18" x="3" y="4" rx="2" ry="2"/>
+                    <line x1="16" x2="16" y1="2" y2="6"/>
+                    <line x1="8" x2="8" y1="2" y2="6"/>
+                    <line x1="3" x2="21" y1="10" y2="10"/>
+                    <path d="M8 14h.01"/>
+                    <path d="M12 14h.01"/>
+                    <path d="M16 14h.01"/>
+                    <path d="M8 18h.01"/>
+                    <path d="M12 18h.01"/>
+                    <path d="M16 18h.01"/>
+                </svg>
+                Calendar
+            </Button>
+            <Button variant="ghost" class="w-full justify-start">
+                <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="mr-2 h-4 w-4"
+                >
+                    <path d="M4.9 19.1C1 15.2 1 8.8 4.9 4.9"/>
+                    <path d="M7.8 16.2c-2.3-2.3-2.3-6.1 0-8.5"/>
+                    <circle cx="12" cy="12" r="2"/>
+                    <path d="M16.2 7.8c2.3 2.3 2.3 6.1 0 8.5"/>
+                    <path d="M19.1 4.9C23 8.8 23 15.1 19.1 19"/>
+                </svg>
+                Radio
+            </Button>
+        </div>
+    </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,19 @@
-<script>
+<script lang="ts">
     import "../app.css";
     import Toaster from "$components/Toaster.svelte";
     import Header from "$components/Header.svelte";
+    import Sidebar from "$components/sidebar.svelte";
 </script>
 
 <div style="height: 100svh" class="wrapped bg-background h-full font-body">
     <Toaster/>
     <Header/>
-    <div class="mx-40 my-5">
-        <slot />
+    <div class="grid lg:grid-cols-5">
+        <Sidebar/>
+        <div class="col-span-3 lg:col-span-4 lg:border-l">
+            <div class="h-full px-4 py-6 lg:px-8">
+                <slot />
+            </div>
+        </div>
     </div>
 </div>
